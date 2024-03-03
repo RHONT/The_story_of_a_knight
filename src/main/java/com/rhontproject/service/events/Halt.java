c671c05983fcd9a6b39994b81da6b09da9cd6371
RHONT <rg915i@yandex.ru>
2024-03-03 18:55:44 +0300

    рефактор класс Halt

--- src/main/java/com/rhontproject/service/events/Halt.java
+++ src/main/java/com/rhontproject/service/events/Halt.java
@@ -7,17 +7,17 @@ import org.springframework.stereotype.Component;
 
 import java.util.Scanner;
 
+import static com.rhontproject.fabrics.global.GlobalVariable.knight;
 import static com.rhontproject.fabrics.global.GlobalVariable.messageService;
 import static java.lang.System.in;
 import static java.lang.System.out;
-
+/**
+ * метод реализующий привал/отдых, когда персонаж подготавливается к следующему вызову
+ */
 @Component
 public class Halt {
-    /**
-     * метод реализующий привал/отдых, когда персонаж подготавливаеться к следующему вызову
-     */
+
     public void halt(Unit unit) {
-        out.println();
         out.println("После битвы вы решили устроить короткий привал");
         messageService.printHealthDefense(unit);
         messageService.printInventory(unit);
@@ -28,7 +28,7 @@ public class Halt {
                 "3 - Отремонтировать на выбор один элемент брони\n" +
                 "0 - Продолжить путешествие\n");
         while (!StateGame.isHalt()) {
-            switchHalt(unit);
+            switchHalt();
         }
         StateGame.setHalt(false);
         StateGame.setCraftIntoHalt(false);
@@ -40,77 +40,84 @@ public class Halt {
     /**
      * вспомогательный метод реализующий функционал метода halt()
      */
-    private void switchHalt(Unit unit) {
+    private void switchHalt() {
+        final String drinkPotion = "1";
+        final String prepareTheShield = "2";
+        final String repair = "3";
+        final String exit = "0";
         Scanner scanHalt = new Scanner(in);
         String tempStr = scanHalt.nextLine();
         switch (tempStr) {
-            case "1":
-                if (unit.getInventorySet().get(InventoryEnum.POTION) > 0) {
-                    for (int i = 0; i < 4; i++) {
-                        unit.attribute.curHealth[i] += 70;
-                    }
-                    unit.getInventorySet().use(InventoryEnum.POTION);
-                    out.println("Вы выпили зелье! Теперь ваше здовровье");
-                    unit.stabilizeHealth();
-                    messageService.printHealthDefense(unit);
-                    out.println("Что еще хотите сделать?");
-                    break;
+            case drinkPotion:
+                if (knight.getInventorySet().get(InventoryEnum.POTION) > 0) {
+                    drinkingPotion();
                 } else {
                     out.println("У вас нет зелья!");
-                    break;
                 }
-
-            case "2":
-                if (unit.getDefenseWall().getDurability() == 0) {
-
-                    if (unit.getDefenseWall().loadShield()) {
+                break;
+            case prepareTheShield:
+                if (knight.getDefenseWall().getDurability() == 0) {
+                    if (knight.getDefenseWall().loadShield()) {
                         out.println("Вы приготовили щит к следующему бою");
-                        break;
                     } else {
                         out.println("У вас нет щитов");
-                        break;
                     }
                 } else {
-                    out.println("Ваш щит уже приготовлен, его прочность: " + unit.getDefenseWall().getDurability());
-                    break;
-                }
-
-            case "3":
-                if (StateGame.isCraftIntoHalt())
-                    out.println("На привале можно починить лишь один элемент брони. Вы уже этим воспользовались");
-                LABEL_1:
-                while (!StateGame.isCraftIntoHalt()) {
-                    out.println("Что будем чинить? 1-2-3-4? Прочность элемента повышаеться на 30");
-                    Scanner strCraft = new Scanner(in);
-                    String strCraftArmor = strCraft.nextLine();
-                    switch (strCraftArmor) {
-                        case "1":
-                        case "2":
-                        case "3":
-                        case "4": {
-                            int curIndexArmor=Integer.parseInt(strCraftArmor)-1;
-                            unit.attribute.defense[curIndexArmor] += 30;
-                            StateGame.setCraftIntoHalt(true);
-                            out.println("Доспех починен! Что выберете еще?");
-                            break;
-                        }
-                        default:
-                            out.println("Введено недопустимое значение");
-                            out.println();
-                            continue LABEL_1;
-                    }
-                    messageService.printDefense(unit);
-                    break;
+                    out.println("Ваш щит уже приготовлен, его прочность: " + knight.getDefenseWall().getDurability());
                 }
                 break;
 
-            case "0": {
+            case repair:
+                if (StateGame.isCraftIntoHalt()) {
+                    out.println("На привале можно починить лишь один элемент брони. Вы уже этим воспользовались");
+                } else {
+                    repairArmor();
+                }
+                break;
+            case exit: {
                 StateGame.setHalt(true);
                 break;
             }
-
             default:
                 out.println("Введено недопустимое значение");
+        }
+    }
+
+    private void drinkingPotion() {
+        for (int i = 0; i < 4; i++) {
+            knight.attribute.curHealth[i] += 70;
+        }
+        knight.getInventorySet().use(InventoryEnum.POTION);
+        out.println("Вы выпили зелье! Теперь ваше здоровье:");
+        knight.stabilizeHealth();
+        messageService.printHealthDefense(knight);
+        out.println("Что еще хотите сделать?");
+    }
+
+    private void repairArmor() {
+        out.println("Что будем чинить? 1-2-3-4? Прочность элемента повышается на 30");
+        Scanner strCraft = new Scanner(in);
+        int digitValue;
+        while (!StateGame.isCraftIntoHalt()) {
+            String strCraftArmor = strCraft.nextLine();
+            digitValue=returnCheckedDigit(strCraftArmor);
+            if (digitValue >= 0 && digitValue < 4) {
+                knight.attribute.defense[digitValue] += 30;
+                StateGame.setCraftIntoHalt(true);
+                out.println("Доспех починен! Что выберете еще?");
+                break;
+            } else {
+                out.println("Введено недопустимое значение");
+            }
+        }
+        messageService.printDefense(knight);
+    }
+
+    private int returnCheckedDigit(String strCraftArmor){
+        try {
+            return Integer.parseInt(strCraftArmor) - 1;
+        } catch (NumberFormatException e){
+            return -1;
         }
     }
 }
